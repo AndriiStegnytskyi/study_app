734393015ec692f567ffe3803dbb7ade5eaf1218
AndriiStegnytskyi <a.stegnytskyi@gmail.com>
2020-12-16 22:17:14 +0100

    Add tests to User model

--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -3,9 +3,8 @@ require 'test_helper'
 class BlogTest < ActiveSupport::TestCase
 
 	def setup 
-		@user = User.new(name: "Andrii Stegnytskyi", email: "a.stegnytskyi@gmail.com")
+		@user = User.new(name"Andrii Stegnytskyi", email: "a.stegnytskyi@gmail.com")
 		@blog = Blog.new(content: "Some content of my first micropost", user_id: @user.id)
 	end
-
 
 end
