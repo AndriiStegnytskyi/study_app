e50d36942230bcbbe5bead3452b2955974562e7c
AndriiStegnytskyi <a.stegnytskyi@gmail.com>
2020-12-18 16:45:52 +0100

    Added static pages controller / contact page

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+	
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+
+end
