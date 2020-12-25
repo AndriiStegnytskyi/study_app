bcdaa5bb9c18862207b05fb5d2f85119cb1c4d7d
AndriiStegnytskyi <a.stegnytskyi@gmail.com>
2020-12-25 18:47:12 +0100

    Added Blogs pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,9 @@
 class BlogsController < ApplicationController
-  def index
-  	@blogs = Blog.paginate(page: params[:page], :per_page => 9)
+  def index 	
+  	@blogs = Blog.paginate(page: params[:page], :per_page => 9, :total_entries =>9)
   end
 
   def show
+		@blogs = Blog.paginate(page: params[:page], :per_page => 9)
   end
 end
