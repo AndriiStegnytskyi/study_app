f902e5374f161d234acd71896acbb85179103e8d
AndriiStegnytskyi <a.stegnytskyi@gmail.com>
2021-01-09 10:37:31 +0100

    Getting prepared for user's authentification implementation

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,21 +4,6 @@ class BlogsController < ApplicationController
   	@blogs = Blog.paginate(page: params[:page], :per_page => 9, :total_entries =>9)
   end
 
-  def create
-    user = User.new(user_params)
-    user.save!
-    blog = user.blogs.build(blog_params)
-    blog.save!
-  end
-
-  def blog_params 
-  	params.require(:blog).permit(:content)
-  end
-
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
-
   def show
 	@blogs = Blog.paginate(page: params[:page], :per_page => 9)
   end
